c95fab5989010f757bf0e73b1ca3ce15573d71b9
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-03-07 07:31:06 +0900

    [master] replace nuxt composition api

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
         あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。<br />
         またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い巨きな石の建物のなかで考えていると、みんなむかし風のなつかしい青い幻燈のように思われます。では、わたくしはいつかの小さなみだしをつけながら、しずかにあの年のイーハトーヴォの五月から十月までを書きつけましょう。
       </p>
+      <p>{{ hoge }}</p>
       <ul>
         <li v-for="user in users" :key="user.name">
           {{ `${user.name}: ${user.email}` }}
@@ -20,7 +21,7 @@
 
 <script lang="ts">
 import gql from 'graphql-tag'
-import { defineComponent, watch } from '@vue/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   apollo: {
@@ -32,6 +33,10 @@ export default defineComponent({
         }
       }
     `,
+  },
+  setup() {
+    const hoge = 'asd'
+    return { hoge }
   },
 })
 </script>
